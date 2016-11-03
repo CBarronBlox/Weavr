cf8202c8fbaa4607cdfd4c9a6e2bceeb52d6dd53
CWestBlox <cwestblox@gmail.com>
2016-11-03 13:23:12 -0500

    Add files via upload

--- systemjs.config.js
+++ systemjs.config.js
@@ -41,7 +41,7 @@
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
       'ts':                        'npm:plugin-typescript@4.0.10/lib/plugin.js',
       'typescript':                'npm:typescript@2.0.2/lib/typescript.js',
-
+      'gojs':                      'npm:gojs'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
